25ef8d9f270d164b7d796acdea0e37da4bf2f86f
Dhananjoy Biswas <ebappa1971@gmail.com>
2019-06-20 00:12:16 +0600

    Updates

---
+++ precache-manifest.da071ebb2a6b887de13d80e32c4d25e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f62481e10519bbaa4c4918ec4cfbda88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71e3260b2f9dd2a9babd",
+    "url": "/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "0b34fa4cca35c4098d8c",
+    "url": "/static/js/2.d93fff74.chunk.js"
+  },
+  {
+    "revision": "71e3260b2f9dd2a9babd",
+    "url": "/static/js/main.fc3498fc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
